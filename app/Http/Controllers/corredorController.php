329a4a35c4741cce491b15b4efcae105e84b0e68
moanjimenez87 <moanjimenez@utp.edu.co>
2017-03-22 17:23:01 -0500

    las librerias de los controladores

--- app/Http/Controllers/corredorController.php
+++ app/Http/Controllers/corredorController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\corredores;
+
+use Session;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class corredorController extends Controller
 {
     public function create(Request $request)
@@ -13,8 +19,19 @@ class corredorController extends Controller
 
     public function store(Request $request)
  {
+  $this->validate($request, [
+ 'name' => 'required | string | alpha_dash | max:66',
+ 'email' => 'required | email',
+ 'password' => 'required | string | min:8 | max:64',
+ ]);
+
+
  $input = $request->all();
  User::create($input);
+
+	Session::flash('flash_message', 'User successfully added!');
+
+
  return redirect('/home');
  }
 
