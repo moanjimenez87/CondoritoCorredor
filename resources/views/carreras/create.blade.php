a3f9b51e499b73ba11ebfb8bdf00c0c313d83a94
moanjimenez87 <moanjimenez@utp.edu.co>
2017-04-06 18:43:44 -0500

    modificacion de aspectos de ingreso

--- resources/views/carreras/create.blade.php
+++ resources/views/carreras/create.blade.php
@@ -3,8 +3,36 @@
 
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+<!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="/imagenes/paisaje1.png" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Campeonato de Ciclismo "Condorito Corredor".</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
 
-<p class="lead">Set the information of the new user.</p>
+       <hr class="featurette-divider">
+
+<p class="lead">Por Favor Ingrese la Información del circuito</p>
 <hr>
 
 @if($errors->any())
@@ -18,9 +46,9 @@
 <form method="post" action="/carreras">
 
 <div class="form-group">
- {!! Form::label('name', 'Name', ['class' => 'control-label']) !!}
+ {!! Form::label('name', 'Nombre de la Carrera', ['class' => 'control-label']) !!}
  {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
-{!! Form::submit('Create New User', ['class' => 'btn btn-primary']) !!}
+{!! Form::submit('Guardar Carrera', ['class' => 'btn btn-primary']) !!}
 {!! Form::close() !!}
 @stop
--- resources/views/carreras/index.blade.php
+++ resources/views/carreras/index.blade.php
@@ -2,9 +2,38 @@
 
 @section('content')
 
-<h1>Users List</h1>
-<p class="lead">Here's a list of all your users.
-    <a href="{!! url('carreras/create') !!}">Add a new one?</a></p>
+ <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="/imagenes/paisaje1.png" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Campeonato de Ciclismo "Condorito Corredor".</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+
+       <hr class="featurette-divider">
+
+<h1>Lista de las Carreras Inscritas</h1>
+<p class="lead">Estas son las Carreras programadas.
+    <a href="{!! url('carreras/create') !!}">Desea Ingresar una nueva ruta?</a></p>
 <hr>
 
 @foreach($list as $corredor)
