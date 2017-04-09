4daebd69eb7d894d38a996572b0a93b70140bf31
moanjimenez87 <moanjimenez@utp.edu.co>
2017-04-09 14:28:11 -0500

    presentacion de la inscripcion carrera

--- resources/views/rutas/create.blade.php
+++ resources/views/rutas/create.blade.php
@@ -32,8 +32,6 @@
 
        <hr class="featurette-divider">
 
-<p class="lead">Por Favor Ingrese la Información del circuito</p>
-<hr>
 
 @if($errors->any())
     <div class="alert alert-danger">
@@ -43,28 +41,51 @@
     </div>
 @endif
 
+
+
+<?php $content = DB::table('carreras')->select('name')->get(); ?>
+
+@foreach($content as $contenido)
+
+  <hr class="featurette-divider">
+
+<div class="container">
+  <h2>Listas de Carreras</h2>
+  <p> Estas son la lista de las carreras activas.</p>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Nombre de la Carrera</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="success">
+        <td>{{ $contenido->name }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+    @endforeach
+
+<hr class="featurette-divider">
+
 <form method="post" action="/rutas">
 
-<div class="form-group">
- {!! Form::label('nombreUsuario', 'Nombre de la Carrera', ['class' => 'control-label']) !!}
- {!! Form::text('nombreUsuario', null, ['class' => 'form-control']) !!}
+<div class="container">
+  <h2>Inscripción</h2>
+    <div class="form-group">
+     {!! Form::label('nombreUsuario', 'Por favor Escriba su nombre completo:', ['class' => 'control-label']) !!}
+     {!! Form::text('nombreUsuario', null, ['class' => 'form-control']) !!}
+    </div>
+     <div class="form-group">
+     {!! Form::label('NombreCarrera', 'Nombre de la Carrera que desea participar:', ['class' => 'control-label']) !!}
+     {!! Form::text('NombreCarrera', null, ['class' => 'form-control']) !!}
+    </div>
+    {!! Form::submit('Guardar Carrera', ['class' => 'btn btn-primary']) !!}
+    {!! Form::close() !!}
+
 </div>
- <div class="form-group">
- {!! Form::label('NombreCarrera', 'Nombre de la Carrera', ['class' => 'control-label']) !!}
- {!! Form::text('NombreCarrera', null, ['class' => 'form-control']) !!}
-</div>
-
-{!! Form::submit('Guardar Carrera', ['class' => 'btn btn-primary']) !!}
-{!! Form::close() !!}
-
-
-$corredor = DB::carrera('name')->get();
-
-foreach ($corredor as $corredor)
-{
-    echo $corredor->name;
-}
-
 
 
 @stop
